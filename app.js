f08979c555187c32485ff5795da4a5cc692fa7cc
matias <matcardillo91@gmail.com>
2022-01-19 17:16:42 -0300

    fsdf

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express(); 
-
+var methodOverride=require('method-override');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var fs=require("fs")
-
+let usuarios=fs.readFileSync('ingreso.json',{encoding:'utf8'});
+let usuario=JSON.parse(usuarios)
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('operaciones')
@@ -20,10 +21,10 @@ router.post('/', function(req, res, next) {
  }else {
  tipo="egreso"
  }
- 
+ let id=Number(op.usuario[op.usuario.length-1].id)+1
  
  var nuevo=
-               {consepto,
+               {id,consepto,
                monto,
                fecha,
             tipo}
@@ -41,6 +42,37 @@ router.post('/', function(req, res, next) {
   
   
   res.redirect('/')
+});
+
+router.get('/editar', function(req, res, next) {
+  res.render('editar',{usuario})
+});
+router.get('/:id', function(req, res, next) {
+  var op = new Array();
+ 
+  op=JSON.parse(fs.readFileSync('ingreso.json',{encoding:'utf8'}))
+ var operacion=op.usuario[req.params.id-1]
+  res.render('edicion',{operacion});
+});
+
+router.put('/:id',function(req, res, next) {
+  var array = new Array();
+ 
+ array=JSON.parse(fs.readFileSync('ingreso.json',{encoding:'utf8'}))
+ nuevo
+ 
+
+ fs.writeFile("ingreso.json",JSON.stringify(array),error=>{
+  if(error){
+    console.log("error")
+   
+  }
+  else
+  console.log("funciona")
+})
+ 
+ res.redirect("/")
+ 
 });
 module.exports = router;
 
